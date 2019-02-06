a4a49f0f1016122e340a6ae05611ad2e8d32d075
jpcarmona <jpcarmona92@gmail.com>
2019-02-06 22:27:27 +0100

    Intento 2 version 2.6

--- juanpe/api/connect.php
+++ juanpe/api/connect.php
@@ -6,7 +6,8 @@ $pass = getenv('MYSQL_PASSWORD');
 $conn = mysqli_connect($host, $user, $pass);
 if (!$conn) {
     exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
+} else {
+	echo 'Successful database connection!'.PHP_EOL;
 }
 
-echo 'Successful database connection!'.PHP_EOL;
 ?>
